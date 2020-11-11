feb3b373fa072e06c5afa04a2f3a31c333d470b8
JOHN YUAN <John.YCQ@outlook.com>
2020-11-11 16:07:20 +0800

    feat: format html with prettier

--- src/middleware/htmlMiddleware.ts
+++ src/middleware/htmlMiddleware.ts
@@ -1,5 +1,6 @@
 import path from 'path'
 import ejs from 'ejs'
+import prettier from 'prettier'
 import { Request, Response, NextFunction } from 'express'
 import { Config } from '../config'
 
@@ -21,7 +22,6 @@ export const htmlMiddleware = (options: Options) => {
       ejs.renderFile(htmlPath, templateGlobals, (err, str) => {
         if (err) {
           const code = (err as { code?: string }).code
-
           if (code === 'ENOENT') {
             next()
           } else {
@@ -34,7 +34,28 @@ export const htmlMiddleware = (options: Options) => {
             console.error(err)
           }
         } else {
-          res.send(str)
+          try {
+            const htmlDir = path.dirname(htmlPath)
+            const prettierConfigFile = prettier.resolveConfigFile.sync(htmlDir)
+            const prettierConfig = prettierConfigFile
+              ? (prettier.resolveConfig.sync(prettierConfigFile) || {})
+              : {}
+
+            const formatted = prettier.format(str, {
+              ...prettierConfig,
+              parser: 'html'
+            })
+
+            res.send(formatted)
+          } catch (err) {
+            res.status(500)
+            res.setHeader('Content-type', 'text/plain; charset=utf-8')
+            res.send(`Error: Failed to format ${filename}\n\n` +
+              `${err.message}\n\nPlease check the log for more details.`)
+
+            console.error(`Error: Failed to format ${htmlPath}`)
+            console.error(err)
+          }
         }
       })
     } else {
